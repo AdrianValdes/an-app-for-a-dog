e4eb29180933aac9ed5f70941a3641f52f650a16
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-04-03 21:10:04 +0200

    Updates

---
+++ precache-manifest.c716f5a3cc9c30951adc0e47531ec63e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885e431397021e17b7a9569bb78b1d08",
+    "url": "/an-app-for-a-dog/index.html"
+  },
+  {
+    "revision": "2071146c654525f4252d",
+    "url": "/an-app-for-a-dog/static/css/main.32c94421.chunk.css"
+  },
+  {
+    "revision": "6ca126c8fa54e4a28f3c",
+    "url": "/an-app-for-a-dog/static/js/2.5f4e8f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/an-app-for-a-dog/static/js/2.5f4e8f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2071146c654525f4252d",
+    "url": "/an-app-for-a-dog/static/js/main.7a3679af.chunk.js"
+  },
+  {
+    "revision": "b32825e1cfc3b8ad4501",
+    "url": "/an-app-for-a-dog/static/js/runtime-main.c801f1de.js"
+  },
+  {
+    "revision": "79c33d54a301e59e9e514e3ac48b667f",
+    "url": "/an-app-for-a-dog/static/media/unnamed.79c33d54.jpg"
+  }
+]);
